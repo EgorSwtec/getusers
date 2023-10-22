cf2d97c2e914ccbe9159edda2ab4e4c85b4b23ab
Egor Mazur <egor.mazur@wowcube.com>
2023-10-22 21:17:53 +0300

    Initial commit.

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Get_Users"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/getusers/Greeting.kt
@@ -0,0 +1,9 @@
+package com.example.getusers
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
